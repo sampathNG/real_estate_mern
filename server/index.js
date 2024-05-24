f5ebb41e538caf7121ef65eaa7f91edc46c658b0
sampathNG <jnanisampath@gmail.com>
2024-05-24 14:58:20 +0530

    server connected to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import BodyParser from "body-parser";
+import mongoose from "mongoose";
+// import path from "path";
+const dbURL =
+  "mongodb+srv://ramuksampath5:passwords@cluster0.u47vioy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const app = express();
 app.use(cors());
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
-app.listen(3000, () => console.log("Server is running on port 3000"));
+mongoose
+  .connect(`${dbURL}`)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => console.log(err));
+const port = process.env.PORT;
+app.listen(3000, () => console.log(`Server is running on port ${port}`));
